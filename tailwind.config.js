3aa1ed5c3f228ffe1c52423de72e8168d8d4d2b7
mizucode <mizurisysthensis3@gmail.com>
2023-06-03 14:43:27 +0700

    fix bg hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./public/**/*.{html,js}"],
-    theme: {
-        container: {
-            center: true,
-            padding: "16px",
-        },
-        extend: {
-            fontFamily: {
-                poppins: ["Poppins"],
-            },
-            colors: {
-                dsr: "#1455cd",
-                ylw: "#f4bc2b",
-            },
-            screens: {
-                "2xl": "1320px",
-            },
-        },
-        plugins: [],
-    },
+	content: ["./public/**/*.{html,js}"],
+	theme: {
+		container: {
+			center: true,
+			padding: "16px",
+		},
+		extend: {
+			fontFamily: {
+				poppins: ["Poppins"],
+			},
+			colors: {
+				dsr: "#1455cd",
+				ylw: "#f4bc2b",
+				niger: "#212529",
+				biru: "#1390ff",
+				kuning: "#ffd93d",
+				abu: "#454545",
+				dark: "#232323",
+				putih: "#f7f7f7",
+				text: "#393C43",
+			},
+			screens: {
+				"2xl": "1320px",
+			},
+		},
+
+		plugins: [],
+	},
 };
